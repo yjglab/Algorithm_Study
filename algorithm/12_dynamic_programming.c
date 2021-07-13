10253a54c78d54fbf60bb09218bbcbc0595370c8
yjglab <yjgdesign@gmail.com>
2021-07-13 15:44:37 +0900

    Dynamic Programming

--- algorithm/12_dynamic_programming.c
+++ algorithm/12_dynamic_programming.c
@@ -1,16 +1,18 @@
+// Algorithm Analysis
+// 다이나믹 프로그래밍 (Dynamic Programming)
 
 #include <stdio.h>
 
-int i[1001];
+int i[11];
 int fibonacci_dp(int x) {
 	if (x == 1) return 1;
-	if (x == 2) return 2;
+	if (x == 2) return 1;
 	if (i[x] != 0) return i[x];
 	return i[x] = fibonacci_dp(x - 1) + fibonacci_dp(x - 2);
 }
 
 int main() {
-	printf("%d\n", fibonacci_dp(9));
+	printf("%d\n", fibonacci_dp(10));
 
 	return 0;
 }
